e052d1474ac182e4e94b6e813c7cf0470e6d1700
onekill-kr <52688566+onekill-kr@users.noreply.github.com>
2019-07-09 13:28:22 +0900

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+mkdir Nukkit
+cd Nukkit
+wget https://ci.nukkitx.com/job/NukkitX/job/Nukkit/view/default/job/master/lastSuccessfulBuild/artifact/target/nukkit-1.0-SNAPSHOT.jar
+wget https://raw.githubusercontent.com/onekill-kr/nukkit-installer/master/start.sh
+wget https://raw.githubusercontent.com/onekill-kr/nukkit-installer/master/loop.sh
+chmod 777 start.sh
+chmod 777 loop.sh
